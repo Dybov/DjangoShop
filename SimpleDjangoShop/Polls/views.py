72525c2b4c649b952f8943fa96ae35a6121e8bd6
Andrey Dybov <dybov.andrew@gmail.com>
2017-07-02 23:22:15 +0500

    Add behavior to detail url
    Add template detail and modify view for rendering

--- SimpleDjangoShop/Polls/views.py
+++ SimpleDjangoShop/Polls/views.py
@@ -1,34 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-#from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.template import loader
-from django.template.context import RequestContext
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-publication_date')[:5]
-    q = latest_question_list[0]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    
-    template = loader.get_template('Polls/index.django.html')
-    cont = {
-        'latest_question_list' : latest_question_list,
-    }
-    
-    return HttpResponse(template.render(cont))
+    context = {"latest_question_list" : latest_question_list}
+    return render(request,'Polls/index.django.html', context)
 
 
 def detail(request, question_id):
-    response = "Detailed info about %s" % question_id
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'Polls/detail.django.html', {"question":question})
+
 
 def results(request, question_id):
     response = "Results of question %s" % question_id
     return HttpResponse(response)
+
 
 def vote(request, question_id):
     response = "You're voting on question %s" % question_id
