874155c87cedc9e9b050a4b1c99125b052c022e0
Andrey Dybov <dybov.andrew@gmail.com>
2017-07-04 20:22:46 +0500

    Escape from hardcoding in templates
    Add app_name in the app urls. Then replace url in template to django tag.

--- SimpleDjangoShop/Polls/urls.py
+++ SimpleDjangoShop/Polls/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+
+app_name = 'Polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
