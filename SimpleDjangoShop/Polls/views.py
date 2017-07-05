a04ac28393776aba3ee448a6fd128729af8b0cf9
Andrey Dybov <dybov.andrew@gmail.com>
2017-07-05 22:35:10 +0500

    Start use ClassBasedViews(CBV) instead of FunctionBasedViews
    Rechange url args to django.generic standart.
    Rewrite views.

--- SimpleDjangoShop/Polls/views.py
+++ SimpleDjangoShop/Polls/views.py
@@ -2,26 +2,30 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect #, HttpResponse
 from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+
 
 from .models import Question, Choice
 
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-publication_date')[:5]
-    context = {"latest_question_list" : latest_question_list}
-    return render(request,'Polls/index.django.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'Polls/detail.django.html', {"question":question})
+class IndexView(generic.ListView):
+    template_name = 'Polls/index.django.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(publication_date__lte=timezone.now()).order_by('-publication_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'Polls/results.django.html',{"question":question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "Polls/detail.django.html"
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "Polls/results.django.html"
 
 
 def vote(request, question_id):
@@ -37,6 +41,18 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('Polls:results', args=(question_id,)))
-    # response = "You're voting on question %s" % question_id
-    # return HttpResponse(response)
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-publication_date')[:5]
+#     context = {"latest_question_list" : latest_question_list}
+#     return render(request,'Polls/index.django.html', context)
+# 
+# 
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'Polls/detail.django.html', {"question":question})
+# 
+# 
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'Polls/results.django.html',{"question":question})
