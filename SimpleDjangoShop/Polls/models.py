fab822894385e958d5637c6510e22ce27ba04f53
Andrey Dybov <dybov.andrew@gmail.com>
2017-07-05 22:38:35 +0500

    Add tests to Question model
    Tests check out if method "was_published_recently" works fine:
    -it doesn't return True when publication_date too old;
    -it doesn't return True when publication_date in the future;
    -it returns True when pub_date was 1 hours ago;

--- SimpleDjangoShop/Polls/models.py
+++ SimpleDjangoShop/Polls/models.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length = 127)
@@ -12,7 +14,9 @@ class Question(models.Model):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.publication_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
