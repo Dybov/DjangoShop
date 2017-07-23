dc7e5dec3406b330ef7a00008a296804ebe44469
Andrey Dybov <dybov.andrew@gmail.com>
2017-07-23 22:46:15 +0500

    Add ugettext function to all strings
    It's necessary for future internationalization.

--- SimpleDjangoShop/JustShop/models.py
+++ SimpleDjangoShop/JustShop/models.py
@@ -9,7 +9,7 @@ from django.core.validators import MinValueValidator
 class Category(models.Model):
 	"""Category class for marking products"""
 	category_name = models.CharField(max_length = 127, verbose_name = _('Имя категории'))
-	category_slug = models.SlugField(max_length = 127, allow_unicode = True , verbose_name = 'Краткое имя')
+	category_slug = models.SlugField(max_length = 127, allow_unicode = True , verbose_name = _('Краткое имя'))
 	
 	def __unicode__(self):
 		return self.category_name
@@ -23,18 +23,18 @@ class Product(models.Model):
 		Category,
 		on_delete = models.PROTECT, #It raises exception when smbd try to delete joined category
 	)
-	name = models.CharField(max_length = 127, verbose_name = 'Название')
-	slug = models.SlugField(max_length = 127, allow_unicode = True, verbose_name = 'Краткое название товара')
-	image = models.ImageField(upload_to = 'products/%Y/%m/%d', blank = True, verbose_name = 'Изображение товара')
-	description = models.TextField(blank=True, verbose_name = 'Описание', )
+	name = models.CharField(max_length = 127, verbose_name = _('Название'))
+	slug = models.SlugField(max_length = 127, allow_unicode = True, verbose_name = _('Краткое название товара'))
+	image = models.ImageField(upload_to = 'products/%Y/%m/%d', blank = True, verbose_name = _('Изображение товара'))
+	description = models.TextField(blank=True, verbose_name = _('Описание'))
 	price = models.DecimalField(decimal_places = 2,
 								max_digits = 6,
-								verbose_name = 'Цена, рубль',
+								verbose_name = _('Цена, рубль'),
 								validators=[
 									MinValueValidator(0.01)],
 	)
-	stock = models.PositiveIntegerField(verbose_name = 'На складе')
-	available = models.BooleanField(default = True, verbose_name = 'Доступен')
+	stock = models.PositiveIntegerField(verbose_name = _('На складе'))
+	available = models.BooleanField(default = True, verbose_name = _('Доступен'))
 	created = models.DateTimeField(auto_now_add = True)
 	updated = models.DateTimeField(auto_now = True)
 	
