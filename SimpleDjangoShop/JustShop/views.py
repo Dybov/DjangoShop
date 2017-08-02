7a8a062afbac6ffd7916c9a118a9bd869dd0e742
Andrey Dybov <dybov.andrew@gmail.com>
2017-08-02 23:00:27 +0500

    Add category to ProductList context
    To show it further in template.

--- SimpleDjangoShop/JustShop/views.py
+++ SimpleDjangoShop/JustShop/views.py
@@ -11,11 +11,16 @@ class ProductList(ListView):
 	context_object_name = 'products'
 	def get_queryset(self):
 		objects = self.model.objects.all()
-		category_slug = self.kwargs.get('category')
-		if category_slug:
-			cat =  get_object_or_404(Category, category_slug = category_slug)
+		self.category_slug = self.kwargs.get('category')
+		if self.category_slug:
+			cat =  get_object_or_404(Category, category_slug = self.category_slug)
 			return objects.filter(category=cat)
 		return objects
+	def get_context_data(self, **kwargs):
+		context = super(ProductList, self).get_context_data(**kwargs)
+		if self.category_slug:
+			context['category'] = self.category_slug
+		return context
 
 class ProductDetail(DetailView):
 	model = Product
