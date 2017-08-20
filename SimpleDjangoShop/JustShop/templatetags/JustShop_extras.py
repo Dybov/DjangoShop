24276bddc2aa3cacf603142731d946a4c7a42bd3
Andrey Dybov <dybov.andrew@gmail.com>
2017-08-20 11:21:59 +0500

    Add template tags for checking current app in navigation bar.
    Add get_current_app_name tag.
    Add is_active_app which returns "" or " active".
    Modify is_active_nav which now uses only one argument - url

--- SimpleDjangoShop/JustShop/templatetags/JustShop_extras.py
+++ SimpleDjangoShop/JustShop/templatetags/JustShop_extras.py
@@ -3,11 +3,24 @@ from __future__ import unicode_literals
 
 from django.shortcuts import reverse
 from django.template.library import Library
+from django.urls import resolve
 register = Library()
 
+def app_name(url):
+    return resolve(url).app_name
 
-@register.simple_tag
-def is_active_nav(request, url):
-    if url == request.path :
-        return "active"
+@register.simple_tag(takes_context=True)
+def is_active_nav(context, url):
+    if url == context["request"].path :
+        return " active"
+    return ""
+
+@register.simple_tag(takes_context=True)
+def get_current_app_name(context):
+    return app_name(context['request'].path)
+
+@register.simple_tag(takes_context=True)
+def is_active_app(context, app_name):
+    if get_current_app_name(context) == app_name:
+        return " active"
     return ""
